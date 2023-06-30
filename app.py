a427a760230c8ee124d6114fe252377bf8c03020
Bryan <31681524+laubryan@users.noreply.github.com>
2023-06-30 19:03:02 -0400

    Implement demo functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+from engine import processor
 
 app = Flask(__name__)
 
@@ -21,7 +22,7 @@ def pageHome():
     return render_template("home.html", pageData=_globalDefs)
 
 # Choose Page
-@app.route("/choose-page")
+@app.route("/choose-page", methods=["POST"])
 def choosePage():
     return render_template("choose-page.html", pageData=_globalDefs)
 
@@ -30,6 +31,7 @@ def choosePage():
 def uploadFile():
     # Get uploaded file
     uploadedFile = request.files["file"]
+    print(uploadedFile)
 
     # Save file
     # TODO: Create unique filename
@@ -40,12 +42,27 @@ def uploadFile():
     
     return redirect("/choose-page")
 
-# Evaluate all languages
-@app.route("/evaluate")
-def pageEvaluateAll():
-    _globalDefs["language"] = "all languages"
-    return render_template("evaluate.html", pageData=_globalDefs)
-
 #
 # API Methods
 #
+
+# Process page image
+@app.route("/process-page-image", methods=["POST"])
+def processPageImage():
+
+    # Get image buffer from client
+    page_image = request.form.get("image-buffer", None)
+    print(f"Received buffer: {len(page_image)} bytes")
+
+    # DEBUG
+    validationData = [
+        { "label": "Short-term investments", "extracted_value": "3799", "original_value": "3799" },
+        { "label": "Accounts receivable", "extracted_value": "926", "original_value": "926" },
+        { "label": "Total current assets", "extracted_value": "7516", "original_value": "7516" },
+        { "label": "Investments deposits and other assets", "extracted_value": "936", "original_value": "936" },
+        { "label": "Deferred income tax", "extracted_value": "134", "original_value": "134" },
+        { "label": "Total current liabilities", "extracted_value": "7775", "original_value": "7775" },
+        { "label": "Total shareholders equity", "extracted_value": "4400", "original_value": "4400" },
+    ]
+
+    return { "validation_data": validationData }
--- static/home.js
+++ static/home.js
@@ -3,6 +3,15 @@
 //
 
 //
+// Accept validations
+//
+function acceptValidation() {
+
+	// Update page state
+	updatePageState(2);
+}
+
+//
 // Browse for file
 //
 function browseForFile() {
@@ -11,7 +20,6 @@ function browseForFile() {
 	fileControl.onchange = () => {
 
 		// Get the selected file
-		let fileLabel = document.getElementById("file-name");
 		let pdfFile = fileControl.files[0];
 
 		if (pdfFile) {
@@ -19,9 +27,11 @@ function browseForFile() {
 			// Create URI
 			fileURI = URL.createObjectURL(pdfFile);
 
-			// Display filename in control
+			// Display file data
+
+			// Filename
 			let filename = fileControl.files[0].name;
-			fileLabel.innerText = filename;
+			displayFilename(filename);
 
 			// Render document
 			renderDocument(fileURI);
@@ -30,7 +40,103 @@ function browseForFile() {
 }
 
 //
-// Test
+// Display filename
+//
+function displayFilename(filename) {
+
+	// Filename
+	let fileLabel = document.getElementById("file-name");
+	fileLabel.innerText = filename;
+}
+
+//
+// Display number of pages
+//
+function displayNumPages(numPages) {
+	let numPagesControl = document.getElementById("num-pages");
+	numPagesControl.innerText = numPages;
+}
+
+//
+// Download data as format
+//
+function downloadData(format) {
+
+	// Get raw data
+	let dataElements = getValidatedData();
+	format = format.toLowerCase();
+	
+	// Download data format
+	if (format == "json") {
+		dataObject = JSON.stringify(dataElements);
+	}
+	if (format == "csv") {
+		const array = [Object.keys(dataElements[0])].concat(dataElements);
+		dataObject = array.map(it => Object.values(it).toString()).join('\n');
+	}
+	saveObjectAsFile("financial-data." + format, dataObject);
+}
+
+//
+// Get validated data
+//
+function getValidatedData() {
+
+	// Assemble validated data
+	let dataElements = [];
+	let container = document.getElementById("validation-content");
+	for (rowElement of container.children) {
+
+		// Get row label
+		let rowLabel = rowElement.dataset.label;
+
+		// Get row value
+		let valueField = rowElement.querySelector("input[type='number']");
+		let rowValue = valueField.value;
+
+		// Add element to list
+		let dataElement = { "label": rowLabel, "value": rowValue }
+		dataElements.push(dataElement);
+	}
+
+	return dataElements;
+}
+
+//
+// Populate validation data
+//
+async function populateValidationData(response) {
+
+	// Convert response to json data
+	let jsonData = await response.json();
+	let validationData = jsonData["validation_data"];
+
+	// Render rows
+	let container = document.getElementById("validation-content")
+	for (const[i, row] of validationData.entries()) {
+
+		// Define row HTML
+		let rowHtml = `
+		<row class="validation-row" data-index="row-${i}" data-label="${row.label}">
+			<span class='line-item-label'>${row.label}</span>
+			<img src="/static/images/debug-field-value.png">
+			<button id="btn-back" class="button-playback" title="RW"><img src="/static/images/play.png"></button>
+			<input type="number" value="${row.extracted_value}">
+			<input type="checkbox">
+		</row>
+		`;
+
+		// Create row element
+		let rowElement = document.createElement("template");
+		rowElement.innerHTML = rowHtml;
+
+		// Add row to container
+		container.appendChild(rowElement.content);
+	}
+}
+
+//
+// Render PDF document
 //
 function renderDocument(filename) {
 
@@ -38,6 +144,9 @@ function renderDocument(filename) {
 	let loadingTask = pdfjsLib.getDocument(filename);
 	loadingTask.promise.then(pdf => {
 
+		// Display number of pages
+		displayNumPages(pdf.numPages);
+		
 		// Load target page
 		pdf.getPage(10).then(page => {
 
@@ -69,9 +178,119 @@ function renderDocument(filename) {
 			// Render page
 			let renderTask = page.render(renderContext);
 			renderTask.promise.then(() => {
+
+				// Convert canvas image to string
 				let dataUri = canvas.toDataURL("image/png");
-				console.log(dataUri);
+
+				// Save image data to field
+				let bufferField = document.getElementById("image-buffer");
+				bufferField.value = dataUri;
 			});
 		});
 	});
+}
+
+//
+// Reset and restart
+//
+function restart() {
+
+	// Reset controls
+
+	// File control
+	let fileControl = document.getElementById("file-control");
+	fileControl.value = "";
+
+	// Filename
+	displayFilename("");
+
+	// Pages
+	displayNumPages("-");
+
+	// Canvas
+	let canvas = document.getElementById("pdf-view");
+	let context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Validation Rows
+	let container = document.getElementById("validation-content");
+	container.innerHTML = "";
+
+	// Update page state
+	updatePageState(0);
+}
+
+//
+// Save object as file
+//
+function saveObjectAsFile(filename, dataObject) {
+	
+	const blob = new Blob([dataObject], { type: "text/json" });
+	const link = document.createElement("a");
+
+	link.download = filename;
+	link.href = window.URL.createObjectURL(blob);
+	link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
+
+	const evt = new MouseEvent("click", {
+			view: window,
+			bubbles: true,
+			cancelable: true,
+	});
+
+	link.dispatchEvent(evt);
+	link.remove()
+}
+
+//
+// Submit the selected page
+//
+async function submitPage() {
+
+	// Update page state
+	updatePageState(1);
+
+	// Get form object for submission
+	let pageForm = document.getElementById("choose-page-form");
+	let formData = new FormData(pageForm);
+
+	// Get the image buffer data
+	let bufferField = document.getElementById("image-buffer");
+	let imageData = bufferField.value;
+	
+	// Add the image buffer to the form data
+	formData.append("page_image", imageData);
+
+	// Submit the form
+	fetch("/process-page-image", { method: "POST", body: formData }).then(response => {
+		if (response.ok) {
+			// Success
+			populateValidationData(response);
+		}
+	}).catch(error => {
+		// Error
+		console.log("Error");
+		console.log(response);
+	});
+}
+
+//
+// Update visible page sections
+//
+function updatePageState(visibleSectionIndex) {
+
+	// Get section references
+	let sections = [
+		document.getElementById("choose-page"),
+		document.getElementById("validate-results"),
+		document.getElementById("download-data"),
+	];
+
+	// Hide all sections
+	for (section of sections) {
+		section.style.display = "none";
+	}
+
+	// Show the target section
+	sections[visibleSectionIndex].style.display = "block";
 }
