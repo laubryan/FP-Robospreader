84b9fa258a8867e435fcf5a05f3dccc340e4afb8
Bryan <31681524+laubryan@users.noreply.github.com>
2023-08-13 17:24:35 -0400

    Add sequential audio read and interrupt

--- static/scripts/test.js
+++ static/scripts/test.js
@@ -2,14 +2,92 @@
 // Test Page Functions
 //
 
+// Capture stop audio click
+var interruptClicked = false;
+var currentRowIndex = -1;
+
+//
+// User interrupted audio playback
+//
+function interruptAudio(event) {
+
+	// Set interrupt flag
+	interruptClicked = true;
+
+	// Stop audio
+	let currentAudio = document.querySelector("#audio-" + currentRowIndex);
+	if (currentAudio) {
+
+		// Stop audio
+		currentAudio.pause();
+		currentAudio.currentTime = 0;
+
+		// Reset cell image transforms
+		let imageField = document.getElementById("cell-image-" + currentRowIndex);
+		imageField.style = "";
+	}
+
+	// Call finished handler explicitly
+	// (for situations where the audio was interrupted)
+	onAudioFinished(currentRowIndex);
+
+	return false;
+}
+
+//
+// Audio finished playing
+//
+function onAudioFinished(rowIndex) {
+	
+	// Check if interrupt was clicked
+	if (interruptClicked) {
+
+		// Reset interrupt flag
+		interruptClicked = false;
+
+		// Remove interrupt listener
+		removeEventListener("mousedown", interruptAudio);
+
+		// Focus interrupted field
+		let interruptedField = document.querySelector("#audio-" + rowIndex + " ~ input");
+		if (interruptedField) {
+
+			// Shade field background
+			interruptedField.style.background = "#ffe0e0";
+
+			// Put cursor in field
+			interruptedField.select();
+			window.setTimeout(() => interruptedField.focus(), 0);
+		}
+	}
+	else {
+
+		// No interrupt, so play next audio
+		nextRowId = rowIndex + 1;
+		playAudio(nextRowId);
+	}
+}
+
 //
 // Play audio sample
 //
 function playAudio(rowIndex) {
 
+	// Set interrupt handler
+	addEventListener("mousedown", interruptAudio, true);
+	currentRowIndex = rowIndex;
+
+	// Clear highlighting and selection from all input fields
+	inputFields = document.querySelectorAll("#validation-content input");
+	for (inputField of inputFields) {
+		inputField.style = "";
+		inputField.blur();
+	}
+
 	// Get control references
 	let audioSample = document.getElementById("audio-" + rowIndex)
 	let imageField = document.getElementById("cell-image-" + rowIndex);
+	if (!audioSample) return;
 
 	// Highlight image field
 	imageField.style = "border: 2px solid red; transform: scale(1.4);";
@@ -18,6 +96,9 @@ function playAudio(rowIndex) {
 	audioSample.onended = () => {
 		imageField.style = "";
 	}
+
+	// Set finished event handler
+	audioSample.addEventListener("ended", () => onAudioFinished(rowIndex), false);
 
 	// Play the audio
 	audioSample.play();
