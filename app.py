65f01ced5268095f4b298085fca0d74476963d81
Bryan <31681524+laubryan@users.noreply.github.com>
2023-08-19 23:25:02 -0400

    Move test files to separate project

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import sqlite3
-
-from db import db
 from engine import processor
-from engine import test
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
@@ -14,7 +10,6 @@ app.config["UPLOAD_EXTENSIONS"] = [".pdf"]
 # Global Definitions
 _globalDefs = {
     "appname": "RoboSpreader",
-    "db": sqlite3.connect("db/test.db")
 }
 
 #
@@ -25,30 +20,6 @@ _globalDefs = {
 @app.route("/")
 def pageHome():
     return render_template("home.html", pageData=_globalDefs)
-
-# Test
-@app.route("/test", methods=["GET"])
-def pageTest():
-
-    # Get test data
-    test1_data, test2_data = test.get_test_data()
-    
-    return render_template("test.html", pageData=_globalDefs, test1_data=test1_data, test2_data=test2_data)
-
-##### DEBUG ####
-
-# Initialize 
-@app.route("/initialize", methods=["GET"])
-def initialize():
-    db.initialize()
-    table_data = [db.get_table("fields"), db.get_table("tests")]
-    return render_template("status.html", pageData=_globalDefs, tables=table_data)
-
-# Results
-@app.route("/results", methods=["GET"])
-def show_results():
-    table_data = [db.get_table("results")]
-    return render_template("status.html", pageData=_globalDefs, tables=table_data)
 
 #
 # Errors
@@ -90,26 +61,3 @@ def createAudioString():
     audio_string = processor.create_audio_string(value_string)
 
     return audio_string
-
-#
-# API Methods for Test
-#
-
-# Record test results
-@app.route("/record-test-results", methods=["POST"])
-def recordTestResults():
-
-    # Get test result data
-    test_id = int(request.form.get("test-id", -1))
-    test_number = int(request.form.get("test-number"))
-    start_time = request.form.get("test-start-time")
-    end_time = request.form.get("test-end-time")
-    input_values = request.form.get("test-values").split(",")
-
-    # Calculate elapsed time
-    elapsed_time_ms = int(end_time) - int(start_time)
-
-    # Save test results to db
-    test_id = test.save_test_results(test_id, test_number, elapsed_time_ms, input_values)
-
-    return { "test_id": str(test_id) }
